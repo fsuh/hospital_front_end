2c5334257176eca82ae84d6ce1d83831f7efad02
tuan <rickytuan1991@gmail.com>
2022-11-18 05:26:48 +0200

    add new btn

---
+++ src/component/button/BtnSubmit.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BtnSubmit(props) {
+  return <button className='type="submit" className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"'>{props?.message}</button>;
+}
